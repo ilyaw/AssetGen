bfbb2fde9dfd687da827c5daceb06b99529b74db
Ilya.Rudenko <ilya.rudenko@waveaccess.global>
2022-06-18 20:42:58 +0300

    Init

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "AssetGen",
+        dependencies: [
+            .package(url: "https://github.com/kylef/Commander.git", .upToNextMajor(from: "0.9.1")),
+            .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "1.0.1"))
+        ])
